ff7017f5fa0fe6e19531161b675e90fb809037a2
brucostam <brucostam@gmail.com>
2016-10-22 18:38:08 -0200

    products are now shown as figures in a chat with an ID link

--- samples/botStoreGenesis.py
+++ samples/botStoreGenesis.py
@@ -62,16 +62,33 @@ class CreateShopBot(telepot.helper.ChatHandler):
                 bot.sendMessage(chat_id, 'Choose the product type:', reply_markup=markup)
                 
             # Show Category Product
+            # elif text in self.product_types:
+            #     keyboardCurrentProductsBtns = []
+            #     for product in self.products:
+            #         string = product.to_json()
+            #         data = json.loads(string)
+            #         if data['product']['product_type'] == text:
+            #             keyboardCurrentProductsBtns.append([KeyboardButton(text=data['product']['title'])])
+            #     # text = ''
+            #     markup = ReplyKeyboardMarkup(keyboard=keyboardCurrentProductsBtns, one_time_keyboard=True)
+            #     bot.sendMessage(chat_id, 'Choose product: ', reply_markup=markup)
+
+            # Show Category Product As Texts
             elif text in self.product_types:
-                keyboardCurrentProductsBtns = []
+                bot.sendMessage(chat_id, 'Click on product code to select: ')
+
                 for product in self.products:
                     string = product.to_json()
                     data = json.loads(string)
                     if data['product']['product_type'] == text:
-                        keyboardCurrentProductsBtns.append([KeyboardButton(text=data['product']['title'])])
-                # text = ''
-                markup = ReplyKeyboardMarkup(keyboard=keyboardCurrentProductsBtns, one_time_keyboard=True)
-                bot.sendMessage(chat_id, 'Choose product: ', reply_markup=markup)
+                        image = data['product']['image']
+                        print image
+                        if image != None:
+                            imageURL = data['product']['image']['src']
+                            bot.sendPhoto(chat_id, imageURL, caption=data['product']['title'] + '\n' + '/' + str(data['product']['id']) + '\n' + data['product']['variants'][0]['price'])
+                        else:
+                            bot.sendMessage(chat_id, data['product']['title'] + '\n' + '/'+str(data['product']['id'])
+                                            + '\n' + data['product']['variants'][0]['price'])
 
             # Show Product Details
             elif text in self.productsNames:
@@ -103,10 +120,40 @@ class CreateShopBot(telepot.helper.ChatHandler):
                                  + '\nPrice: $' + self.currentPriceStr
                 bot.sendMessage(chat_id, detailedString, reply_markup=markup)
 
+            # Show Product Details By Code
+            elif text[0] == '/': #remember to put all / commands before this elif
+                # Name: title
+                #
+                # Type: product_type
+                #
+                # Details: body_html
+                #
+                # Price:$variants[0].price
+                #
+                # Buy  button
+                productById = shopify.Product.find(text[1:])
+                string = productById.to_json()
+                data = json.loads(string)
+
+                self.currentNameStr =  data['product']['title']
+                self.currentTypeStr = data['product']['product_type']
+                self.currentDetailStr = data['product']['body_html']
+                self.currentPriceStr = data['product']['variants'][0]['price']
+
+                # text = ''
+                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Pay')]], one_time_keyboard=True)
+
+                detailedString = 'Item: ' + self.currentNameStr \
+                                 + '\nType: ' + self.currentTypeStr \
+                                 + '\nDetails: ' + self.currentDetailStr \
+                                 + '\nPrice: $' + self.currentPriceStr
+                bot.sendMessage(chat_id, detailedString, reply_markup=markup)
+
+
             # Show Payment Confirmation Question
             elif text == 'Pay':
                 # text = ''
-                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Confirm')],[KeyboardButton(text='Cancel')]], one_time_keyboard=True)
+                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Confirm'),KeyboardButton(text='Cancel')]], one_time_keyboard=True)
 
                 confirmationString = 'Item: ' + self.currentNameStr + ' $' + self.currentPriceStr \
                                      + '\n\nConfirm purchase?'
@@ -123,10 +170,14 @@ class CreateShopBot(telepot.helper.ChatHandler):
 
 
 #keep this order
-API_KEY = sys.argv[1]
-PASSWORD = sys.argv[2]
-SHOP_NAME = sys.argv[3]
-TOKEN = sys.argv[4]
+#API_KEY = sys.argv[1]
+API_KEY = "f0238210a5210edbd943d49526cba54e"
+#PASSWORD = sys.argv[2]
+PASSWORD = "f50bd1e9c9e87befc956ac0677acd359"
+#SHOP_NAME = sys.argv[3]
+SHOP_NAME = "vanhackathonstore-2"
+#TOKEN = sys.argv[4]
+TOKEN="260558085:AAFXp5xENhne000dQm9t2Iv8ksq8_XK7ySU"
 
 
 bot = telepot.DelegatorBot(TOKEN, [
